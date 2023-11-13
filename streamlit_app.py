3475c3a6b9def878d97d7266b26daf45c50877b5
rpbvh <150685490+rpbvh@users.noreply.github.com>
2023-11-13 14:43:10 +0100

    added pandas and csv read

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('hela hola kinder cola')
 
@@ -9,3 +10,5 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
