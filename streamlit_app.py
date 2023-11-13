a5757825091cabfe1f39783c4ac5d9d50f87506b
rpbvh <150685490+rpbvh@users.noreply.github.com>
2023-11-13 14:37:29 +0100

    added header andn text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('hela hola kinder cola')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
