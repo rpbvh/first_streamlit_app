cf2cf00068f80cb122bd179ec8fd4221415f95c6
rpbvh <150685490+rpbvh@users.noreply.github.com>
2023-11-14 09:44:16 +0100

    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('hela hola kinder cola')
 
@@ -21,3 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
