bea98cb51f1cab025d3e9c2d1afcb00674be3edf
rpbvh <150685490+rpbvh@users.noreply.github.com>
2023-11-14 16:01:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,8 @@ except URLError as e:
   steamlit.error()
 
 streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
-def get_fruit_load_list()
+
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
     my_cur.execute ("select * from fruit_load_list")
     return my_cur.fetchall()
